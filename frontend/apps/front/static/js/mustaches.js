3ec23be68552257a0f9883febafd042f0f1f5f1e
Danilo Bargen <gezuru@gmail.com>
2011-10-14 22:34:46 +0200

    Fixed mustachify.js

--- frontend/apps/front/static/js/mustaches.js
+++ frontend/apps/front/static/js/mustaches.js
@@ -1,8 +1,6 @@
-base_uri = 'http://gesagt-im-parlament.ch'
-
 $(document).ready(function() {
     $('.person img').each(function() {
         src = $(this).attr('src');
-        $(this).attr('src', 'http://mustachify.me/?src=' + base_uri + src);
+        $(this).attr('src', 'http://mustachify.me/?src=' + src);
     });
 })
