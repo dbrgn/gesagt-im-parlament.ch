512a17cb4ed6fc79ba9f27dfdd06e1fafdb1d501
Danilo Bargen <gezuru@gmail.com>
2012-05-20 00:45:04 +0200

    Added postgresql database settings

--- frontend/settings_prod.py
+++ frontend/settings_prod.py
@@ -6,6 +6,15 @@ from settings import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'dbrgn_parlament',       # Or path to database file if using sqlite3.
+    'USER': '',                      # Not used with sqlite3.
+    'PASSWORD': '',                  # Not used with sqlite3.
+    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+}
+
 INSTALLED_APPS = (
     # Contrib apps
     'django.contrib.staticfiles',
