57d4497040c2f9753480b5e305b8e8d4e730c32c
Danilo Bargen <gezuru@gmail.com>
2012-05-20 02:59:32 +0200

    Static files configuration for production

--- frontend/settings_prod.py
+++ frontend/settings_prod.py
@@ -9,7 +9,7 @@ TEMPLATE_DEBUG = False
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
     'NAME': 'dbrgn_parlament',       # Or path to database file if using sqlite3.
-    'USER': '',                      # Not used with sqlite3.
+    'USER': 'dbrgn_parlament',       # Not used with sqlite3.
     'PASSWORD': '',                  # Not used with sqlite3.
     'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
     'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -25,3 +25,5 @@ INSTALLED_APPS = (
     # 3rd party apps
     'gunicorn',
 )
+
+STATIC_ROOT = '/home/dbrgn/webapps/static/'
